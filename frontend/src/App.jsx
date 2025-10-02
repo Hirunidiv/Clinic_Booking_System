73927da39e7424794ba80eff450dfc70fc8544eb
Hirunidiv <hirunidivyanjalee365@gmail.com>
2025-10-02 15:48:49 +0530

    created Admin Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
+import AdminSideBar from './components/AdminSideBar'
 
 function App() {
   
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <AdminSideBar/>
     </>
   )
 }
